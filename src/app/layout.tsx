003ce8982f316b899a8ce1a2886f2f31da32db1d
Gabriel <flaviogabrielsr0507@gmail.com>
2023-12-25 02:07:41 -0300

    feat: updade code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import 'swiper/css/autoplay';
 import { Inter } from "next/font/google";
 import { Metadata } from "next";
 import Script from "next/script";
+import { Suspense } from "react";
+import Analytics from "./components/analytics";
 
 export const metadata: Metadata = {
 	metadataBase: new URL("https://flaviogabrielportfolio.com.br"),
@@ -59,21 +61,14 @@ export default function RootLayout({
 	return (
 		<html lang="pt-br" className={[inter.variable].join(" ")} suppressHydrationWarning={true}>
 			<head>
-				<Script id="google-tag-manager" strategy="afterInteractive">
-					{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MX64LJX2');`}
-				</Script>
-				<Script src="https://www.googletagmanager.com/gtag/js?id=GTM-MX64LJX2" />
-
 			</head>
 			<body
 				className={` ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
 					}`}
 			>
-				<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MX64LJX2"
-					height="0" width="0" style={{
-						display: 'none',
-						visibility: 'hidden'
-					}}></iframe></noscript>
+				<Suspense>
+					<Analytics />
+				</Suspense>
 				{children}
 			</body>
 		</html >
